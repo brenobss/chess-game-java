3538ff898803c4e01bfdb034413553529a384988
brenobss <brenobarreto5@hotmail.com>
2021-08-07 15:53:19 -0300

    create board in terminal

--- src/application/App.java
+++ src/application/App.java
@@ -1,10 +1,11 @@
 package application;
 
 import boardgame.Position;
+import chess.ChessMatch;
 
 public class App {
     public static void main(String[] args) {
-        Position position = new Position(5, 10);
-        System.out.println(position);
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
     }
 }
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -9,7 +9,7 @@ public class ChessMatch {
         board = new Board(8, 8);
     }
 
-    public ChessPiece[][] getPiece(){
+    public ChessPiece[][] getPieces(){
         ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
         for(int i = 0; i < board.getRows(); i++){
             for (int j = 0; j < board.getColumns(); j++){
